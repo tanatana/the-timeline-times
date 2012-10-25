5efd2d441a093bd19d3f46da96fc1ee4a2a82465
phelrine <phelrine@gmail.com>
2012-10-26 01:35:31 +0900

    undefined method修正

--- app.rb
+++ app.rb
@@ -78,21 +78,21 @@ class App < Sinatra::Base
     # @webpages = Webpage.where(:user_id => u.id, :updated_at.gte => 1.days.ago).sort(:updated_at.desc)
     # @articles = Article.where(:user => u.id).sort(:updated_at.desc).limit(100)
     # @articles = Article.where(:user_id => u.id).sort(:updated_at.desc).limit(100)
-    @articles = Article.pagenate({
-                                   :order => :updated_at.desc,
-                                   :per_page => 30,
-                                   :page => 1,
-                                 })
+    @articles = Article.paginate({
+        :order => :updated_at.desc,
+        :per_page => 30,
+        :page => 1,
+      })
     @articles.each do |article|
       begin
         article.webpage.expanded_url
-      rescue 
+      rescue
         p "help me!"
         p article.id
       end
     end
-    
-    
+
+
     erb :user_home
   end
 
