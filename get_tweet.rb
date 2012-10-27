3cfbd40d584a7dbafb1c04036ffc9c48c7d19a39
tanatana <tugusuke@gmail.com>
2012-10-27 18:32:43 +0900

    同一ツイートが複数回登録される問題を力業で解決

--- get_tweet.rb
+++ get_tweet.rb
@@ -12,6 +12,7 @@ require 'tools/urltoolkit'
 include UrlToolKit
 
 class DocumentIsNOTExist < StandardError; end
+class StatusIsEmpty < StandardError; end
 
 class Hash
   def rename(old_sym, new_sym)
@@ -38,12 +39,16 @@ User.all().each do |curr_user|
                                       consumer,
                                       curr_user.access_token,
                                       curr_user.access_secret))
-
-  latest_status = Status.first(:user_id => curr_user.id, :order => :created_at.desc)
+  # うまく取得数が制限できない
+  # latest_article = Article.first(:user_id => curr_user.id, :order => :updated_at.desc)
+  # p latest_article
   # begin
-  #   raise StatusIsEmpty until latest_status
-  #   tl = rubytter.home_timeline(:since_id => latest_status.status_id, :count => 200, :include_entities => true)
-  # rescue StatusIsEmpty
+  #   raise StatusIsEmpty until latest_article
+  #   latest_status = latest_article.status_ids.first
+  #   p latest_status_id = Status.find(latest_status).status_id
+  #   tl = rubytter.home_timeline(:since_id => latest_status_id, :count => 200, :include_entities => true)
+  # rescue StatusIsEmpty => e
+  #   p e
   #   tl = rubytter.home_timeline(:count => 200, :include_entities => true)
   # end
   tl = rubytter.home_timeline(:count => 200, :include_entities => true)
@@ -55,7 +60,7 @@ User.all().each do |curr_user|
       status.rename(:id_str, :status_id_str)
       status.user.rename(:id, :user_id)
       status.user.rename(:id_str, :user_id_str)
-      mongo_status = Status.new(status)
+      mongo_status = Status.find_or_initialize_by_status_id(status.id)
       mongo_status.save
       urls.each do |url|
         url.remove(:indices)
@@ -64,6 +69,7 @@ User.all().each do |curr_user|
           raise DocumentIsNOTExist if mongo_webpage == nil
           # TODO: make get_title(url), change this
           mongo_webpage.statuses << mongo_status
+          p mongo_webpage.status_ids.size
           mongo_webpage.save
           mongo_article = Article.find_or_initialize_by_user_id_and_webpage_id(curr_user.id, mongo_webpage.id)
           mongo_webpage.articles << mongo_article
@@ -76,7 +82,8 @@ User.all().each do |curr_user|
           mongo_webpage = Webpage.create(url)
           begin
             mongo_webpage.thumb = get_thumb(url.expanded_url)
-          rescue Timeout::Error, Errno::ECONNRESET, EOFError, Errno::ECONNREFUSED
+          rescue Timeout::Error, Errno::ECONNRESET, EOFError, Errno::ECONNREFUSED => e
+            p e
             mongo_webpage.thumb = "http://fakeimg.pl/200x150/"
           end
           mongo_webpage.title = "title"
